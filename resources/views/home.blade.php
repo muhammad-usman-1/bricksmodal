15fccc763e7f0a2fc6575645444887b827ace08b
muhammad-usman-1 <tumhara-github-email@example.com>
2026-01-14 13:58:56 +0500

    s3 functionlity fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -351,7 +351,7 @@ line-height: 36px;
     }
 </style>
 
-<div class="dash-shell">
+<div class="dash-shell" style="padding-top: 20px;">
     {{--  <div class="topbar">
         <div class="search-box">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
